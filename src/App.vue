94720a8590e2ce6f471dd15f0ed1883d55e84caa
tmxhsk99 <tmxhsk99@naver.com>
2022-07-20 05:58:48 +0900

    feat : v-for 활용 nav 추가

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="menu">
+    <a v-for="(a,i) in 메뉴들" :key="i">{{ i }} {{ a }}</a>
+
+  </div>
   <div>
-    <h4>{{products[0]}}</h4>
+    <h4>{{ products[0] }}</h4>
     <p>60 만원</p>
   </div>
   <div>
-    <h4>{{products[1]}}</h4>
+    <h4>{{ products[1] }}</h4>
     <p>70 만원</p>
   </div>
   <div>
-    <h4>{{products[2]}}</h4>
+    <h4>{{ products[2] }}</h4>
     <p>70 만원</p>
   </div>
 </template>
@@ -18,13 +21,13 @@
 
 export default {
   name: 'App',
-  data(){
+  data() {
     return {
-      products : ['역삼동원룸','천호동원룸','마포구원룸']
+      메뉴들: ['Home', 'shop', 'about'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
   },
-  components: {
-  }
+  components: {}
 }
 </script>
 
@@ -35,6 +38,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
