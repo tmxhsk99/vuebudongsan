cb308a25ca7201594ce7487d011608dda8d1f606
tmxhsk99 <tmxhsk99@naver.com>
2022-07-22 22:00:17 +0900

    feat : @click 이벤틑 활용 예제 추가

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="menu">
     <a v-for="(a,i) in menu" :key="i">{{ a }}</a>
-
   </div>
-  <div v-for="(val,i) in products" :key="i">
-    <h4>{{val}}</h4>
-    <p>{{ i+100 }} 만원</p>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>50 만원</p>
+    <button @click="increase(0)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[0] }} </span>
   </div>
+  <div>
+      <h4>{{ products[1] }}</h4>
+      <p>50 만원</p>
+      <button @click="increase(1)">허위매물신고</button>
+      <span>신고수 : {{ 신고수[1] }} </span>
+    </div>
+  <div>
+      <h4>{{ products[2] }}</h4>
+      <p>60 만원</p>
+      <button @click="increase(2)">허위매물신고</button>
+      <span>신고수 : {{ 신고수[2] }} </span>
+    </div>
 </template>
 
 <script>
@@ -15,9 +28,14 @@ export default {
   name: 'App',
   data() {
     return {
+      신고수: [0,0,0],
       menu: ['Home', 'shop', 'about'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
+  },methods : {
+    increase(i){
+      this.신고수[i] += 1;
+    },
   },
   components: {}
 }
