b4af32a729bbdd70245bac7b9402ac866505a333
tmxhsk99 <tmxhsk99@naver.com>
2022-07-31 21:14:25 +0900

    feat : v-if 를 활용한 모달창 추가 및 이미지 추가

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,37 @@
 <template>
+
+  <div class="black-bg" v-if="modalIsOpen == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="modalIsOpen=false">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(a,i) in menu" :key="i">{{ a }}</a>
   </div>
   <div>
-    <h4>{{ products[0] }}</h4>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="modalIsOpen = true">{{ products[0] }}</h4>
     <p>50 만원</p>
     <button @click="increase(0)">허위매물신고</button>
     <span>신고수 : {{ 신고수[0] }} </span>
   </div>
   <div>
-      <h4>{{ products[1] }}</h4>
-      <p>50 만원</p>
-      <button @click="increase(1)">허위매물신고</button>
-      <span>신고수 : {{ 신고수[1] }} </span>
-    </div>
+    <img src="./assets/room1.jpg" class="room-img">
+    <h4>{{ products[1] }}</h4>
+    <p>50 만원</p>
+    <button @click="increase(1)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[1] }} </span>
+  </div>
   <div>
-      <h4>{{ products[2] }}</h4>
-      <p>60 만원</p>
-      <button @click="increase(2)">허위매물신고</button>
-      <span>신고수 : {{ 신고수[2] }} </span>
-    </div>
+    <img src="./assets/room2.jpg" class="room-img">
+    <h4>{{ products[2] }}</h4>
+    <p>60 만원</p>
+    <button @click="increase(2)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[2] }} </span>
+  </div>
 </template>
 
 <script>
@@ -28,12 +40,13 @@ export default {
   name: 'App',
   data() {
     return {
-      신고수: [0,0,0],
+      modalIsOpen: false,
+      신고수: [0, 0, 0],
       menu: ['Home', 'shop', 'about'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
-  },methods : {
-    increase(i){
+  }, methods: {
+    increase(i) {
       this.신고수[i] += 1;
     },
   },
@@ -42,6 +55,34 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
